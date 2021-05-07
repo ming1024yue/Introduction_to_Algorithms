37d2442b87370864b772c9eb403f0b38450e9435
Ming Yue <ming.yue@student.unsw.edu.au>
2021-05-07 15:59:17 +0800

    sort

--- ex6.py
+++
@@ -1,7 +0,0 @@
-
-def convert(s, numRows):
-    
-
-
-if __name__ =="__main__":
-    print()
--- test.py
+++
@@ -1,8 +0,0 @@
-l=[1,1,0,1,0,1]
-res=[]
-for i in l:
-    if i!=0:
-        res.append(i)
-    break
-
-print(res)
